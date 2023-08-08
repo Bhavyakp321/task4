85375666c909e5f913be62172cda3e01675638f7
Bhavyakp321 <141406686+Bhavyakp321@users.noreply.github.com>
2023-08-08 14:59:54 +0530

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,11 +1,8 @@
 import requests
 
 def get_data_from_url(url):
-    try:
         response = requests.get(url)
         print("Success! Response Content")
-    except RequestException as e:
-        print("Error: {e}")
 
 if __name__ == "__main__":
     url = "https://www.google.com"
